c7a8d903f6c524d7a65cfdf79b298669f18ced2e
zackmura <148238477+zackmura@users.noreply.github.com>
2026-04-09 11:17:41 -0300

    Delete ima-nfc/config.js

--- ima-nfc/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const dadosCliente = {
-    nomes: "Arthur & Heitor",
-    foto: "foto.png", // Nome do arquivo da imagem que você vai subir no GitHub
-    fase: "teaser", // Quando o vídeo ficar pronto, mude esta palavra para "video"
-    linkVideoYoutube: "https://www.youtube.com/embed/SEU_CODIGO_AQUI", // Link do vídeo (idealmente não listado no YouTube)
-    mensagemTeaser: "Aguardem... Uma lembrança interativa da Mini Mundo 3D está chegando. Em breve, um vídeo muito especial."
-};
